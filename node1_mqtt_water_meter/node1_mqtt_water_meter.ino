7dfc737f8b13353d0c25cda45aa233bdfd33aa0c
Septiani Laras <69139753+septianilaras99@users.noreply.github.com>
2021-11-14 15:58:38 +0700

    Update node1_mqtt_water_meter.ino
    perubahan kredensial mqtt

--- node1_mqtt_water_meter/node1_mqtt_water_meter.ino
+++ node1_mqtt_water_meter/node1_mqtt_water_meter.ino
@@ -10,14 +10,12 @@
 //kredensial WiFi
 char ssid[] = "Hotspot-laptop";
 char pass[] = "bismillahlulus2021";
-//char ssid[] = "1245";
-//char pass[] = "lalalayeyeye";
 WiFiClient client;
 
 //kredensial publish dan subscribe pada ThingSpeak
-const char mqttUserName[] = "LBw1CTEHBh0bNx40AzAONRA";
-const char clientID[] = "LBw1CTEHBh0bNx40AzAONRA";
-const char mqttPass[] = "PIiVeCuo6DsXI2wIiGxuLBMB";
+const char mqttUserName[] = "change this area";
+const char clientID[] = "change this area";
+const char mqttPass[] = "change this area";
 
 //parameter koneksi 
 const char* server = "mqtt3.thingspeak.com";
